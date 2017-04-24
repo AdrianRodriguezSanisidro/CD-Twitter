5fe1d36ac34bd25fd3a25be26b97e0b9473972da
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-04-24 09:58:22 +0200

    Hice un format para que sea mejor a la hora de revisar el codigo

--- TrabajoTwitter/src/trabajotwitter/Autorizar.java
+++ TrabajoTwitter/src/trabajotwitter/Autorizar.java
@@ -9,15 +9,16 @@ import twitter4j.conf.ConfigurationBuilder;
  * @author Adry
  */
 public class Autorizar {
-    public static Twitter autorizacion(){
+
+    public static Twitter autorizacion() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
-cb.setDebugEnabled(true)
-  .setOAuthConsumerKey("50XsjdZaSSVhh0K89T4cnhueY")
-  .setOAuthConsumerSecret("DriqCDm15djcPZwJ2jL72J6mlG5qFCo8ub4VFJBKoNdoBI66MC")
-  .setOAuthAccessToken("526997779-9dVpwSDGWKFI6EfEvkRr88wNePCPlVy5aAmiIMls")
-  .setOAuthAccessTokenSecret("yJE9FzruR9aL0pRiXe49CcjY2dDrj5MA2ARWZDtzJxQSK");
-TwitterFactory tf = new TwitterFactory(cb.build());
-Twitter twitter = tf.getInstance();
-return twitter;
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("50XsjdZaSSVhh0K89T4cnhueY")
+                .setOAuthConsumerSecret("DriqCDm15djcPZwJ2jL72J6mlG5qFCo8ub4VFJBKoNdoBI66MC")
+                .setOAuthAccessToken("526997779-9dVpwSDGWKFI6EfEvkRr88wNePCPlVy5aAmiIMls")
+                .setOAuthAccessTokenSecret("yJE9FzruR9aL0pRiXe49CcjY2dDrj5MA2ARWZDtzJxQSK");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        return twitter;
     }
 }
