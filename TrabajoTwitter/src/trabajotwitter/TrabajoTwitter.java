92685d8cdb21152430245d0e5041d401eb14f967
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-04-24 08:59:25 +0200

    Quite el metodo autorizacion para ponerlo en una clase a parte

--- TrabajoTwitter/src/trabajotwitter/TrabajoTwitter.java
+++ TrabajoTwitter/src/trabajotwitter/TrabajoTwitter.java
@@ -20,15 +20,4 @@ public class TrabajoTwitter {
     public static void main(String[] args) {
         
     }
-    public void autorizacion(){
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-cb.setDebugEnabled(true)
-  .setOAuthConsumerKey("50XsjdZaSSVhh0K89T4cnhueY")
-  .setOAuthConsumerSecret("DriqCDm15djcPZwJ2jL72J6mlG5qFCo8ub4VFJBKoNdoBI66MC")
-  .setOAuthAccessToken("526997779-9dVpwSDGWKFI6EfEvkRr88wNePCPlVy5aAmiIMls")
-  .setOAuthAccessTokenSecret("	yJE9FzruR9aL0pRiXe49CcjY2dDrj5MA2ARWZDtzJxQSK");
-TwitterFactory tf = new TwitterFactory(cb.build());
-Twitter twitter = tf.getInstance();
-    }
-
 }
