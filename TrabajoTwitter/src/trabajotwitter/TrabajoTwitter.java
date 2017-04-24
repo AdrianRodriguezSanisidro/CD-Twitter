34f0d5c1e4442ae1aa85537ef2afbae1e0596015
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-04-24 09:59:22 +0200

    Añadida la opcion de visualizar la time line,cambiado el string del menu para que muestre la opcion de 2 y hecho un format para que sea mas facil leer el codigo

--- TrabajoTwitter/src/trabajotwitter/TrabajoTwitter.java
+++ TrabajoTwitter/src/trabajotwitter/TrabajoTwitter.java
@@ -1,24 +1,30 @@
-package trabajotwitter; 
+package trabajotwitter;
 
 import javax.swing.JOptionPane;
 import twitter4j.TwitterException;
+
 /**
  *
  * @author Adry
  */
 public class TrabajoTwitter {
 
-    public static void main(String[] args)throws TwitterException  {
+    public static void main(String[] args) throws TwitterException {
         int opcion;
         String nombre;
-        do{
-            opcion=Integer.parseInt(JOptionPane.showInputDialog("1:Publicar twit"));
-            switch(opcion){
-            case 1:
-            Twittear objTwit=new Twittear();
-            objTwit.publicarTwit();
-            break;
-        }
-    }while (opcion!=0);
-}
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("1:Publicar twit\n2:Visualizar time line"));
+            switch (opcion) {
+                case 1:
+                    Twittear objTwit = new Twittear();
+                    objTwit.publicarTwit();
+                    System.out.println("Twit publicado");
+                    break;
+                case 2:
+                    TimeLine objTime = new TimeLine();
+                    objTime.verTimeLine();
+                    System.out.println("****Final de la time line****");
+            }
+        } while (opcion != 0);
+    }
 }
