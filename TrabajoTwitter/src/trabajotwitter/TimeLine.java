4928a4d2eb56cc282bb3342297361b60696c5d63
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-04-24 09:57:46 +0200

    Aumentado la cantidad que puede mostrarse y añadido un system.out.println como separador entre twits

--- TrabajoTwitter/src/trabajotwitter/TimeLine.java
+++ TrabajoTwitter/src/trabajotwitter/TimeLine.java
@@ -13,8 +13,9 @@ public class TimeLine {
     public void verTimeLine() throws TwitterException {
         List<Status> timeLineList = Autorizar.autorizacion().getHomeTimeline();
         System.out.println("Showing home timeline.");
-        for (int i = 0; i <= 30; i++) {
+        for (int i = 0; i <=500; i++) {
             String aux = timeLineList.get(i).getText();
+            System.out.println("-------Twit Nº"+(i+1)+"--------");
             System.out.println(aux);
         }
 
