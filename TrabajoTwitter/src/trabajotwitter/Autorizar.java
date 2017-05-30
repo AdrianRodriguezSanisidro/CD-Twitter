77f8fd067227400326ea5049d8e4273c7fc51d52
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-05-30 20:03:36 +0200

    Hecho el javadoc

--- TrabajoTwitter/src/trabajotwitter/Autorizar.java
+++ TrabajoTwitter/src/trabajotwitter/Autorizar.java
@@ -5,11 +5,14 @@ import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
- *
+ * Clase Autorizar donde estara el metodo que permita establecer conexion con mi cuenta
  * @author Adry
  */
 public class Autorizar {
-
+/**
+ * Contiene las keys y los tokens para hacer la conexion y poder acceder a mi cuenta de twitter
+ * @return twitter
+ */
     public static Twitter autorizacion() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
